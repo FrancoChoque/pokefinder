cf69dee235e85f6b3f890ad5f5945452655824bc
FrancoChoque <franco.choquescipione@gmail.com>
2019-12-09 05:43:34 +0000

    Updates

---
+++ precache-manifest.053f72f7bfa4f9e49272a7c390c66e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8907d67231fa6d3f9fd88e6bd28fb87a",
+    "url": "/pokefinder/index.html"
+  },
+  {
+    "revision": "5d6105f38187dc1eec76",
+    "url": "/pokefinder/static/css/main.bfcce15d.chunk.css"
+  },
+  {
+    "revision": "5467c9f7ac6701e888d5",
+    "url": "/pokefinder/static/js/2.6d70c657.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/pokefinder/static/js/2.6d70c657.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d6105f38187dc1eec76",
+    "url": "/pokefinder/static/js/main.6207ac4c.chunk.js"
+  },
+  {
+    "revision": "5aa80ef743b8ddb1f7a2",
+    "url": "/pokefinder/static/js/runtime-main.8b6f642f.js"
+  },
+  {
+    "revision": "9ba5bec60fb564fb167be613f5c6c677",
+    "url": "/pokefinder/static/media/logo.9ba5bec6.png"
+  }
+]);
